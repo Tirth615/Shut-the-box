f6fdd3413e95ec2cf50fcd0625ed38d0c6a3d4e9
Tirth615 <shahtirth615@gmail.com>
2025-01-07 19:46:34 +0530

    Initial Commit

---
+++ Shut the box/Shut_the_boxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shut_the_boxApp.swift
+//  Shut the box
+//
+//  Created by TirthShah on 07/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Shut_the_boxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
